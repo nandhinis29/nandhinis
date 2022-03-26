ac0cf9d924474164c38b2f328bbcb398018eb808
nandhinis29 <nandhinis29.1994@gmail.com>
2022-03-26 13:33:55 -0700

    felix done his codding

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,7 +2,9 @@ package org.sample;
 
 public class Sample {
 
-	
+	private void add1() {
+		System.out.println("done");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Done");
